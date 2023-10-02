cd81823b14421ea3125904bcd8326e448dcaf3e2
LAYLA GIOVANNA FURTADO DA SILVA <22282096@dsn127m1242376.cfp127aluno.local>
2023-10-02 08:49:25 -0300

    Tela pedidos quase finalizada

--- components/cardPedido.jsx
+++ components/cardPedido.jsx
@@ -4,32 +4,7 @@ import Image from "next/image";
 const CardProdutos = (props) => {
     return (
         <>
-            <div className="flex justify-center flex-col text-center text-text">
-                <span className="text-3xl ">Pedido</span>
-                <span className="text-base">de</span>
-                <div className="flex items-center justify-center w-72">
-                    <div className=" break-all">
-
-                        <p className=" text-base">{props.email}</p>
-                    </div>
-                </div>
-            </div>
-            <div className="grid-cols-2  grid-rows-2 gap-4 mt-4 rounded-lg p-5 w-3/4 h-2/4 bg-grayMedium text-text">
-                <div className="flex  justify-between pb-4">
-                    <div className="">
-                        <div> R$40,00</div>
-                        <div> R$40,00</div>
-                    </div>
-                    <div className="">
-                        <div>1x</div>
-                        <div>1x</div>
-                    </div>
-                </div>
-
-                <div className="flex items-center justify-end ">
-                    Total R$80,00
-                </div>
-            </div>
+            
         </>
     );
 };
--- pages/pedidos.js
+++ pages/pedidos.js
@@ -5,10 +5,11 @@ import CardProdutos from "../components/cardPedido"
 export default function Pedidos() {
   return (
     <Layout>
-      <div className='grid justify-around p-14 flex-col h-full text-white bg-bubble-gum'>
-        <div className="grid grid-cols-3 gap-10 justify-around overflow-auto bg-blue h-full">
+      <div className='grid justify-around p-8 flex-col h-full text-white bg-second overflow-y-auto'>
+        <div className="grid grid-cols-3 gap-10 justify-around   h-full">
 
-          <div className="flex flex-col items-center justify-center rounded-lg shadow-shadow-button  w-80 h-72 bg-white ">
+          {/* Card pedido */}
+          <div className="flex flex-col items-center justify-center rounded-lg shadow-shadow-button   w-80 h-72 bg-white ">
             {/* <CardProdutos email="matheus.siqueira100@gmail.com" products="coxinha"/> */}
             <div className="flex justify-center flex-col text-center text-text">
               <span className="text-3xl ">Pedido</span>
@@ -21,24 +22,38 @@ export default function Pedidos() {
               </div>
             </div>
             <div className="grid-cols-3   grid-rows-2 gap-4 mt-4 rounded-lg p-5 w-3/4 h-2/4 bg-grayMedium text-text">
-              <div className="flex  justify-between pb-4">
-                <div className="">
-                  <div> Coxinha</div>
-                  <div> Coxinha</div>
+              <div className="flex h-4/5 justify-between pb-4">
+                <div className="flex justify-between w-full grid-cols-2 pr-4 overflow-y-auto">
+                  <div className="">
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                  </div>
+                  <div className="">
+                    <div>1x</div>
+                    <div>1x</div>
+                    <div>1x</div>
+                    <div>1x</div>
+                    <div>1x</div>
+                    <div>1x</div>
+                  </div>
+                  
                 </div>
-                
-                <div className="">
-                  <div>1x</div>
-                  <div>1x</div>
-                </div>
+
               </div>
 
-              <div className="flex items-end justify-end bg-pink">
-                Total R$80,00 
+              <div className="flex gap-2 items-end justify-end">
+                Total  <span className="text-primary">R$80,00</span>
               </div>
             </div>
           </div>
-          {/* <div className="flex justify-center flex-col text-center text-text">
+          {/* Card pedido */}
+          <div className="flex flex-col items-center justify-center rounded-lg shadow-shadow-button   w-80 h-72 bg-white ">
+            {/* <CardProdutos email="matheus.siqueira100@gmail.com" products="coxinha"/> */}
+            <div className="flex justify-center flex-col text-center text-text">
               <span className="text-3xl ">Pedido</span>
               <span className="text-base">de</span>
               <div className="flex items-center justify-center w-72">
@@ -48,52 +63,246 @@ export default function Pedidos() {
                 </div>
               </div>
             </div>
-            <div className="mt-4 flex rounded-lg p-5 w-3/4 h-2/4 bg-grayMedium text-text">
-              oi layla voce esta programando um card
-            </div> */}
+            <div className="grid-cols-3   grid-rows-2 gap-4 mt-4 rounded-lg p-5 w-3/4 h-2/4 bg-grayMedium text-text">
+              <div className="flex h-4/5 justify-between pb-4">
+                <div className="flex justify-between w-full grid-cols-2 pr-4 overflow-y-auto">
+                  <div className="">
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                  </div>
+                  <div className="">
+                    <div>1x</div>
+                    <div>1x</div>
+                    <div>1x</div>
+                    <div>1x</div>
+                    <div>1x</div>
+                    <div>1x</div>
+                  </div>
+                  
+                </div>
 
-          {/* <div className="w-80 h-72 bg-midnight">
-            teste card
+              </div>
+
+              <div className="flex gap-2 items-end justify-end">
+                Total  <span className="text-primary">R$80,00</span>
+              </div>
+            </div>
           </div>
-          <div className="w-80 h-72 bg-midnight">
-            teste card
+          {/* Card pedido */}
+          <div className="flex flex-col items-center justify-center rounded-lg shadow-shadow-button   w-80 h-72 bg-white ">
+            {/* <CardProdutos email="matheus.siqueira100@gmail.com" products="coxinha"/> */}
+            <div className="flex justify-center flex-col text-center text-text">
+              <span className="text-3xl ">Pedido</span>
+              <span className="text-base">de</span>
+              <div className="flex items-center justify-center w-72">
+                <div className=" break-all">
+
+                  <p className=" text-base">matheus.siqueira100@gmail.com</p>
+                </div>
+              </div>
+            </div>
+            <div className="grid-cols-3   grid-rows-2 gap-4 mt-4 rounded-lg p-5 w-3/4 h-2/4 bg-grayMedium text-text">
+              <div className="flex h-4/5 justify-between pb-4">
+                <div className="flex justify-between w-full grid-cols-2 pr-4 overflow-y-auto">
+                  <div className="">
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                  </div>
+                  <div className="">
+                    <div>1x</div>
+                    <div>1x</div>
+                    <div>1x</div>
+                    <div>1x</div>
+                    <div>1x</div>
+                    <div>1x</div>
+                  </div>
+                  
+                </div>
+
+              </div>
+
+              <div className="flex gap-2 items-end justify-end">
+                Total  <span className="text-primary">R$80,00</span>
+              </div>
+            </div>
           </div>
-          <div className="w-80 h-72 bg-midnight">
-            teste card
+          {/* Card pedido */}
+          <div className="flex flex-col items-center justify-center rounded-lg shadow-shadow-button   w-80 h-72 bg-white ">
+            {/* <CardProdutos email="matheus.siqueira100@gmail.com" products="coxinha"/> */}
+            <div className="flex justify-center flex-col text-center text-text">
+              <span className="text-3xl ">Pedido</span>
+              <span className="text-base">de</span>
+              <div className="flex items-center justify-center w-72">
+                <div className=" break-all">
+
+                  <p className=" text-base">matheus.siqueira100@gmail.com</p>
+                </div>
+              </div>
+            </div>
+            <div className="grid-cols-3   grid-rows-2 gap-4 mt-4 rounded-lg p-5 w-3/4 h-2/4 bg-grayMedium text-text">
+              <div className="flex h-4/5 justify-between pb-4">
+                <div className="flex justify-between w-full grid-cols-2 pr-4 overflow-y-auto">
+                  <div className="">
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                  </div>
+                  <div className="">
+                    <div>1x</div>
+                    <div>1x</div>
+                    <div>1x</div>
+                    <div>1x</div>
+                    <div>1x</div>
+                    <div>1x</div>
+                  </div>
+                  
+                </div>
+
+              </div>
+
+              <div className="flex gap-2 items-end justify-end">
+                Total  <span className="text-primary">R$80,00</span>
+              </div>
+            </div>
           </div>
-          <div className="w-80 h-72 bg-midnight">
-            teste card
+          {/* Card pedido */}
+          <div className="flex flex-col items-center justify-center rounded-lg shadow-shadow-button   w-80 h-72 bg-white ">
+            {/* <CardProdutos email="matheus.siqueira100@gmail.com" products="coxinha"/> */}
+            <div className="flex justify-center flex-col text-center text-text">
+              <span className="text-3xl ">Pedido</span>
+              <span className="text-base">de</span>
+              <div className="flex items-center justify-center w-72">
+                <div className=" break-all">
+
+                  <p className=" text-base">matheus.siqueira100@gmail.com</p>
+                </div>
+              </div>
+            </div>
+            <div className="grid-cols-3   grid-rows-2 gap-4 mt-4 rounded-lg p-5 w-3/4 h-2/4 bg-grayMedium text-text">
+              <div className="flex h-4/5 justify-between pb-4">
+                <div className="flex justify-between w-full grid-cols-2 pr-4 overflow-y-auto">
+                  <div className="">
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                  </div>
+                  <div className="">
+                    <div>1x</div>
+                    <div>1x</div>
+                    <div>1x</div>
+                    <div>1x</div>
+                    <div>1x</div>
+                    <div>1x</div>
+                  </div>
+                  
+                </div>
+
+              </div>
+
+              <div className="flex gap-2 items-end justify-end">
+                Total  <span className="text-primary">R$80,00</span>
+              </div>
+            </div>
           </div>
-          <div className="w-80 h-72 bg-midnight">
-            teste card
+          {/* Card pedido */}
+          <div className="flex flex-col items-center justify-center rounded-lg shadow-shadow-button   w-80 h-72 bg-white ">
+            {/* <CardProdutos email="matheus.siqueira100@gmail.com" products="coxinha"/> */}
+            <div className="flex justify-center flex-col text-center text-text">
+              <span className="text-3xl ">Pedido</span>
+              <span className="text-base">de</span>
+              <div className="flex items-center justify-center w-72">
+                <div className=" break-all">
+
+                  <p className=" text-base">matheus.siqueira100@gmail.com</p>
+                </div>
+              </div>
+            </div>
+            <div className="grid-cols-3   grid-rows-2 gap-4 mt-4 rounded-lg p-5 w-3/4 h-2/4 bg-grayMedium text-text">
+              <div className="flex h-4/5 justify-between pb-4">
+                <div className="flex justify-between w-full grid-cols-2 pr-4 overflow-y-auto">
+                  <div className="">
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                  </div>
+                  <div className="">
+                    <div>1x</div>
+                    <div>1x</div>
+                    <div>1x</div>
+                    <div>1x</div>
+                    <div>1x</div>
+                    <div>1x</div>
+                  </div>
+                  
+                </div>
+
+              </div>
+
+              <div className="flex gap-2 items-end justify-end">
+                Total  <span className="text-primary">R$80,00</span>
+              </div>
+            </div>
           </div>
-          <div className="w-80 h-72 bg-midnight">
-            teste card
+          {/* Card pedido */}
+          <div className="flex flex-col items-center justify-center rounded-lg shadow-shadow-button   w-80 h-72 bg-white ">
+            {/* <CardProdutos email="matheus.siqueira100@gmail.com" products="coxinha"/> */}
+            <div className="flex justify-center flex-col text-center text-text">
+              <span className="text-3xl ">Pedido</span>
+              <span className="text-base">de</span>
+              <div className="flex items-center justify-center w-72">
+                <div className=" break-all">
+
+                  <p className=" text-base">matheus.siqueira100@gmail.com</p>
+                </div>
+              </div>
+            </div>
+            <div className="grid-cols-3   grid-rows-2 gap-4 mt-4 rounded-lg p-5 w-3/4 h-2/4 bg-grayMedium text-text">
+              <div className="flex h-4/5 justify-between pb-4">
+                <div className="flex justify-between w-full grid-cols-2 pr-4 overflow-y-auto">
+                  <div className="">
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                    <div> Coxinha</div>
+                  </div>
+                  <div className="">
+                    <div>1x</div>
+                    <div>1x</div>
+                    <div>1x</div>
+                    <div>1x</div>
+                    <div>1x</div>
+                    <div>1x</div>
+                  </div>
+                  
+                </div>
+
+              </div>
+
+              <div className="flex gap-2 items-end justify-end">
+                Total  <span className="text-primary">R$80,00</span>
+              </div>
+            </div>
           </div>
-          <div className="w-80 h-72 bg-midnight">
-            teste card
-          </div>
-          <div className="w-80 h-72 bg-midnight">
-            teste card
-          </div>
-          <div className="w-80 h-72 bg-midnight">
-            teste card
-          </div>
-          <div className="w-80 h-72 bg-midnight">
-            teste card
-          </div>
-          <div className="w-80 h-72 bg-midnight">
-            teste card
-          </div>
-          <div className="w-80 h-72 bg-midnight">
-            teste card
-          </div>
-          <div className="w-80 h-72 bg-midnight">
-            teste card
-          </div>
-          <div className="w-80 h-72 bg-midnight">
-            teste card
-          </div> */}
+          
 
         </div>
       </div>
